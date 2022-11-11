6a069ff8eb4f19b085cf2325e5f437276c877765
DaianaSimao <daiana.xd11@hotmail.com>
2022-11-11 00:54:08 -0300

    Criando novo método para index.

--- covid19/app/controllers/home_controller.rb
+++ covid19/app/controllers/home_controller.rb
@@ -1,24 +1,19 @@
 class HomeController < ApplicationController
-  before_action :set_global_sumary_service
+  before_action :set_global_sumary_service 
   def index
     @sumary_service = @sumary_service.general
   end
 
-  def confirmed
-    @confirmed = @sumary_service.confirmed
+  def data
+    @confirmed = @sumary_service.data
   end
 
-  def recovered
-    @recovered = @sumary_service.recovered
-  end
 
-  def deaths
-    @deaths = @sumary_service.deaths
-  end
 
   private
 
   def set_global_sumary_service
     @sumary_service = GlobalSumary.new
   end
+
 end
