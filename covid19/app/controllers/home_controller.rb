cccef5577ed4340b95290d76ab9f042881ca6331
DaianaSimao <daiana.xd11@hotmail.com>
2022-11-14 13:40:12 -0300

    Editando as informçôes da index

--- covid19/app/controllers/home_controller.rb
+++ covid19/app/controllers/home_controller.rb
@@ -1,14 +1,21 @@
 class HomeController < ApplicationController
-  before_action :set_global_sumary_service 
+  before_action :set_global_sumary_service
+  before_action :set_summary_service
   def index
-    @sumary_service = @sumary_service.general
+    @sumary_service2 = @sumary_service2.general2
   end
 
-  def data
-    @confirmed = @sumary_service.data
-  end
+ def data 
+  @data = @sumary_service.data
+ end
 
+ def data2
+  @data2 = @sumary_service2.data2
+ end
 
+ def confirmed
+  @sumary_service = @sumary_service.general
+ end
 
   private
 
@@ -16,4 +23,7 @@ class HomeController < ApplicationController
     @sumary_service = GlobalSumary.new
   end
 
+  def set_summary_service
+    @sumary_service2 = Summary.new
+  end
 end
