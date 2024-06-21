7c80b9f3f393a6edf7da90e38c67a25932877bbd
DaianaSimao <daiana.xd11@hotmail.com>
2024-06-21 12:48:20 -0300

    Deleta  arquivos

--- covid19/config/environment.rb
+++
@@ -1,8 +0,0 @@
-# Load the Rails application.
-require_relative 'application'
-
-# Initialize the Rails application.
-Rails.application.initialize!
-
-Time::DATE_FORMATS[:pais] = "posted on %b, %m %Y - %I:%M%p"
-Time::DATE_FORMATS[:mundo] = "posted on %b, %m %Y - %I:%M%p"
